81e8c445c7c2af27ed0f677da561681ddbb87055
khuanita <khuanita@makebetter.co.za>
2022-10-10 11:12:23 +0200

    deploy

---
+++ build/precache-manifest.725458a20e6cf2db1efdf6741e518b89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c15a863bddb541401c15d2f3672f621",
+    "url": "/portfolio2022/index.html"
+  },
+  {
+    "revision": "4eb504a8610a004ae102",
+    "url": "/portfolio2022/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "9d51ec59549bca9fe4d2",
+    "url": "/portfolio2022/static/css/main.38c16b0b.chunk.css"
+  },
+  {
+    "revision": "4eb504a8610a004ae102",
+    "url": "/portfolio2022/static/js/2.511e205f.chunk.js"
+  },
+  {
+    "revision": "45dd6a8eb784f4bfc84b6e54cbce993a",
+    "url": "/portfolio2022/static/js/2.511e205f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d51ec59549bca9fe4d2",
+    "url": "/portfolio2022/static/js/main.ec991c30.chunk.js"
+  },
+  {
+    "revision": "b257cc5263d9b4c36a11",
+    "url": "/portfolio2022/static/js/runtime-main.1d2e4ed2.js"
+  }
+]);
